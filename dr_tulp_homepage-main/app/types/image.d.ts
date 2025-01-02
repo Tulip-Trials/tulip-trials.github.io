29348c4ef6912eb0aec38882cf303fa1386fa3a9
cole-tulip <146778757+cole-tulip@users.noreply.github.com>
2025-01-02 14:42:14 -0800

    Delete dr_tulp_homepage-main directory

--- dr_tulp_homepage-main/app/types/image.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.jpg' {
-    const content: {
-      src: string;
-      height: number;
-      width: number;
-      blurDataURL?: string;
-    };
-    export default content;
-  }
